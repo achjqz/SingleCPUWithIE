df933ce52580940b43b0593790b56d4d01cb7e92
sunnyboy <18179643146@163.com>
2018-12-26 08:18:59 +0800

    Change Inst

--- Inst_mem.v
+++ Inst_mem.v
@@ -8,17 +8,15 @@ module INSTMEM(Addr, Inst);
 	assign Ram[5'h03] = 32'h3C028000; //lui R2 , 0x1111
 	assign Ram[5'h04] = 32'h00411820; //add R3 , R1 , R2
 	assign Ram[5'h05] = 32'h10220001; //beq R2 , R1 , 1
-	assign Ram[5'h07] = 32'h00222020;  //add R4 , R1 , R2
-	assign Ram[5'h08] = 32'h00222824; // and  R5, R1, R2
+	assign Ram[5'h07] = 32'h00222824; // and  R5, R1, R2
+	assign Ram[5'h08] = 32'h00210826;  //xor R1 R1 R1
 	assign Ram[5'h09] = 32'h14610001; // bne R3 , R1 , 1
 	assign Ram[5'h0A] = 32'h00222020;  //add R4 , R1 , R2
-	assign Ram[5'h0B] = 32'h00223025;  // or R6 , R1 , R2
-	assign Ram[5'h0C] = 32'h00222022;  // sub R4 , R1 , R2
-	assign Ram[5'h0D] = 32'h00221826; //xor R3 , R1 , R2
+	assign Ram[5'h0B] = 32'h0000000c;  // syscall
+	assign Ram[5'h0C] = 32'h00412822;  // sub R4 , R2 , R1
+	assign Ram[5'h0D] = 32'hFFFFFFFF; // XXX
 	assign Ram[5'h0E] = 32'h00021880; //sll R3 , R2 , 2
 	assign Ram[5'h0F] = 32'h00021882; //srl R3 , R2 , 2
-
-
 	assign Ram[5'h10] = 32'h400ba800; //mfc0 R29, Cause
 	assign Ram[5'h11] = 32'h401c7000; //mfc0 R28,EPC;
 	assign Ram[5'h12] = 32'h239C0004; //addi R28, R28, 4;
